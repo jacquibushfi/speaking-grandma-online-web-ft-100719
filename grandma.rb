2e70327e6657a18323e6c01c3eb6c6bd6e249e39
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-07 16:08:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,6 +17,7 @@ def speak_to_grandma(speak)
      puts "I LOVE YOU TOO PUMPKIN!"
      elsif speak = speak.upcase
      puts "NO NOT SINCE 1938!"
+     
    else
        puts "HUH?! SPEAK UP, SONNY!"
        puts "HUH?! SPEAK UP, SONNY!"
